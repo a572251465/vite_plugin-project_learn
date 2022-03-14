49d9bd6e0cd67c29a169efdb9d620a8ed81511c1
lihaohao <2495041749@qq.com>
2022-03-14 22:39:13 +0800

    feat: 配置配置共同文件env 等

--- vue-project/App.vue
+++ vue-project/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import $font from '@/assets/css/font.module.css'
 
 const name = ref<string>('李浩浩')
 </script>
 
 <template>
-  <div>我的姓名是：{{ name }}</div>
+  <div :class="$font.font">我的姓名是：{{ name }}</div>
 </template>
--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const resolvePath = (url: string) => path.resolve(__dirname, url)
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolvePath('src')
+    }
+  },
   plugins: [vue()]
 })
